4eb29f256ca2de4c98e3110facc104d19c9f0eca
Android Wonder <jim.pepe@verizon.net>
2022-04-05 06:41:57 -0400

    add sound to button

--- app/src/main/java/com/course/example/shakybuttons/MainActivity.java
+++ app/src/main/java/com/course/example/shakybuttons/MainActivity.java
@@ -2,6 +2,7 @@ package com.course.example.shakybuttons;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AnimationUtils;
@@ -13,20 +14,27 @@ public class MainActivity extends AppCompatActivity {
 
     private Button button, button2, button3;
     private Animation shake, bounce, rotate;
+    private MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //gewt references to the buttons on UI
         button = (Button)findViewById(R.id.button);
         button2 = (Button)findViewById(R.id.button2);
         button3 = (Button)findViewById(R.id.button3);
 
+        //attach animation scripts
         shake = AnimationUtils.loadAnimation(this, R.anim.shake);
         bounce = AnimationUtils.loadAnimation(this, R.anim.bounce);
         rotate = AnimationUtils.loadAnimation(this, R.anim.rotate);
 
+        //create media player for sound on one button
+        mp = MediaPlayer.create(this, R.raw.dingdong);
+
+        //set button listeners
         button.setOnClickListener( new OnClickListener(){
             public void onClick(View v){
                 button.startAnimation(shake);
@@ -39,9 +47,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //animation and sound
         button3.setOnClickListener( new OnClickListener(){
             public void onClick(View v){
                 button3.startAnimation(rotate);
+                mp.seekTo(0);
+                mp.start();
             }
         });
     }
